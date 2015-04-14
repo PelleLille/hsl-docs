5e874efce64203be94ebebc10a413dbdc23cd81b
Erik Lax <erik@halon.se>
2015-04-14 15:51:42 +0200

    lala

--- conf.py
+++ conf.py
@@ -116,7 +116,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'lib']
+exclude_patterns = ['_build', 'lib', 'lib64']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
