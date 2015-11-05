441eee0957d13e280bec52f0c177d6cf2f758574
Erik Lax <erik@halon.se>
2015-11-05 10:46:38 +0100

    Added tools

---
+++ tools/funcsort.php
@@ -0,0 +1,27 @@
+<?php
+$file = file("functions.rst");
+$last = null;
+
+function dump()
+{
+	global $name, $list;
+	if (empty($list)) return;
+	echo "* **$name**";
+	foreach($list as $l) echo " :func:`$l`";
+	echo "\n";
+	$list = array();
+}
+
+foreach($file as $f)
+{
+	if ($f[0] == '-') {
+		dump();
+		$name = trim($last);
+	}
+	if (preg_match("/function[:][:] (.*?)[(]/", $f, $func))
+		$list[] = $func[1];
+	$last = $f;
+}
+
+dump();
+?>
