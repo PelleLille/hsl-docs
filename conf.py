814d641e5fcad2e5b140213cd023667d9e9e0e72
Erik Lax <erik@halon.se>
2017-09-05 10:55:10 +0200

    bump date

--- conf.py
+++ conf.py
@@ -117,7 +117,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Halon Scripting Language'
-copyright = u'2016, Halon Security'
+copyright = u'2017, Halon Security'
 author = u'Halon Security'
 
 # The version info for the project you're documenting, acts as replacement for
